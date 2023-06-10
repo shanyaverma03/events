39fa68eafdcce604b4bcd04b17d8af75a1e73c54
Shanya <shanyaverma03@gmail.com>
2023-06-10 19:47:02 +0530

    add reusable button and add in event item

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import classes from "./EventItem.module.css";
+import Button from "../UI/Button";
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -25,7 +26,7 @@ const EventItem = (props) => {
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={`/events/${id}`}> Explore Event </Link>
+          <Button link={`/events/${id}`}>Explore Event</Button>
         </div>
       </div>
     </li>
