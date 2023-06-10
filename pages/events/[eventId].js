4777ecd56b943ef3b066992a9eda27b60892fd68
Shanya <shanyaverma03@gmail.com>
2023-06-10 20:18:26 +0530

    show info about each event id

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,8 @@
 import { useRouter } from "next/router";
 import { getEventById } from "@/dummy-data";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
 
 const EventDetail = () => {
   const router = useRouter();
@@ -13,8 +16,16 @@ const EventDetail = () => {
 
   return (
     <div>
-      <h1>Event detail page</h1>
-      <p>{event.title}</p>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
     </div>
   );
 };
