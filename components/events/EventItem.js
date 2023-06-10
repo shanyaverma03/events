92c1b5c0375ee2e7fbef513018c305f32d0541dc
Shanya <shanyaverma03@gmail.com>
2023-06-10 20:05:02 +0530

    add icons- heroicons

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import classes from "./EventItem.module.css";
 import Button from "../UI/Button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -19,14 +22,22 @@ const EventItem = (props) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanRedableDate}</time>
           </div>
+
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={`/events/${id}`}>Explore Event</Button>
+          <Button link={`/events/${id}`}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
